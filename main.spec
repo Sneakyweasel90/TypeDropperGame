cca431879a7618937f969446ba72104302fe6d0a
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-08-14 11:01:18 -0300

    created exe file for single run

--- main.spec
+++ main.spec
@@ -5,7 +5,7 @@ a = Analysis(
     ['main.py'],
     pathex=[],
     binaries=[],
-    datas=[],
+    datas=[('words.txt', '.')],
     hiddenimports=[],
     hookspath=[],
     hooksconfig={},
--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+import os
+import sys
+
+def resource_path(relative_path):
+    try:
+        #this needs to be here for grabbing pyinstaller file
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 def load_words(filepath):
-    with open(filepath, "r") as f:
+    full_path = resource_path(filepath)
+    with open(full_path, "r") as f:
         return [line.strip() for line in f if line.strip()]
