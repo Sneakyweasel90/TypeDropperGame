f3bf6b806233f6a2d95526afdc4ea1ed3b914d38
Sneakyweasel90 <neil.mannion1@gmail.com>
2025-08-25 13:56:03 -0300

    changed settings

--- settings.py
+++ settings.py
@@ -1,11 +1,21 @@
 import pygame
 from utils import resource_path
 
-WIDTH, HEIGHT = 800, 600
+pygame.init()
+
+info = pygame.display.Info()
+SCREEN_WIDTH, SCREEN_HEIGHT = info.current_w, info.current_h
+
+WIDTH = int(SCREEN_WIDTH * 0.8)
+HEIGHT = int(SCREEN_HEIGHT * 0.8)
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-FALL_SPEED = 2
+
+FALL_SPEED = int(HEIGHT * 0.003)
+
 BACKGROUND_IMAGE = pygame.image.load(resource_path("assets/TypeDropper.png"))
+BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (WIDTH, HEIGHT))
 
 pygame.font.init()
-FONT = pygame.font.Font(None, 48)
+FONT = pygame.font.Font(None, int(HEIGHT * 0.08))
